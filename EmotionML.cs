441dc9004664ed62ba58062d54fa281d04f75f99
Gerhard Fobe <coding@gerhard-fobe.de>
2012-08-03 00:29:34 +0200

    add MIME-type and file extension out of Candidate Recommendation

--- EmotionML.cs
+++ EmotionML.cs
@@ -18,6 +18,16 @@ namespace EmotionML
         public const string PRAEFIX = "emo";
 
         /// <summary>
+        /// MIME media type of EmotionML
+        /// </summary>
+        public const string MIME_TYPE = "application/emotionml+xml";
+
+        /// <summary>
+        /// File-extension of EmotionML
+        /// </summary>
+        public const string FILE_EXTENSION = "emotionml";
+
+        /// <summary>
         /// Version of highest implemented EmotionML-Recommendation
         /// </summary>
         public const string VERSION = "1.0 - Working Draft 2011-04-07";
@@ -26,5 +36,9 @@ namespace EmotionML
         /// Version of this EmotionML-Library
         /// </summary>
         public const string LIBRARY_VERSION = "1.0alpha";
+
+        //TODO: XML-Schemata vom W3C
+        //http://www.w3.org/TR/2012/CR-emotionml-20120510/emotionml.xsd
+        //http://www.w3.org/TR/2012/CR-emotionml-20120510/emotionml-fragments.xsd
     }
 }
